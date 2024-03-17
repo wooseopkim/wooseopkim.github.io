8137304aca197ae1992707adecbee8050fb77599
Wooseop Kim <wooseop@protonmail.com>
2024-03-17 18:37:23 +0900

    Enable JSX types with ESLint

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/* eslint-enable @typescript-eslint/triple-slash-reference */
+
+import 'astro/astro-jsx';
+
+declare global {
+  namespace JSX {
+    type Element = HTMLElement;
+    // type Element = astroHTML.JSX.Element // We want to use this, but it is defined as any.
+    type IntrinsicElements = astroHTML.JSX.IntrinsicElements;
+  }
+}
